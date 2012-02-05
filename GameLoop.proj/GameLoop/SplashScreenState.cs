1f1c81b786f632cf286eba365ba09b12b8d37867
Jean-Paul Roberts <jean_pr@yahoo.com>
2012-02-05 22:20:47 +0000

    GameLoop renamed to GameLoop.proj

---
+++ GameLoop.proj/GameLoop/SplashScreenState.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GameLoop
+{
+    class SplashScreenState : IGameObject
+    {
+
+        StateSystem _system;
+
+        public SplashScreenState(StateSystem system)
+        {
+            _system = system;
+        }
+
+        public void Update(double elapsedTime)
+        {
+            System.Console.WriteLine("Updating Splash");
+        }
+
+        public void Render()
+        {
+            System.Console.WriteLine("Rendering Splash");
+        }
+    }
+}
---
+++ GameLoop.proj/GameLoop/Test_StateSystem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace GameLoop
+{
+    [TestFixture]
+    class Test_StateSystem
+    {
+        [Test]
+        public void TestAddedStateExists()
+        {
+            StateSystem stateSystem = new StateSystem();
+            stateSystem.AddState("splash", new SplashScreenState(stateSystem));
+
+            // Does the added function now exist?
+
+            Assert.IsTrue(stateSystem.Exists("splash"));
+        }
+    }
+}
